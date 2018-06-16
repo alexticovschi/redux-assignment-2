08c76c28352f65bbfe2ecd45be260cd9790a692a
AlexTicovschi <alexmticovschi@gmail.com>
2018-06-16 11:37:26 +0100

    assignment solution

---
+++ src/store/reducer.js
@@ -0,0 +1,31 @@
+import * as actionType from './actions';
+ 
+const initialState = {
+    persons: []
+}
+ 
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionType.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: 'Alex',
+                age: Math.floor( Math.random() * 40 )
+            }
+            return {
+                ...state,
+                persons: state.persons.concat(newPerson)
+            }
+        case actionType.DELETE_PERSON:
+            const updatedArray = state.persons.filter(person => person.id !== action.personId)
+                return { 
+                    ...state,
+                    persons: updatedArray
+                }
+        default:
+            return state
+    }
+}
+ 
+ 
+export default reducer;
